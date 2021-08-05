a9d4b585e4980b6443de77701b0d29269f82991e
kristianreyes7 <kristianreyes7@icloud.com>
2021-08-05 15:52:59 -0700

    Date switched to string

---
+++ server 2.js
@@ -0,0 +1,28 @@
+//====dependencies====//
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+//====middleware====//
+app.use(cors());
+app.use(express.json());
+
+//====controllers====//
+const movieController = require('./controllers/movie_controller.js');
+app.use('/movies', movieController);
+
+//====server====//
+app.listen(PORT, () => {
+  console.log('listening...', PORT);
+})
+mongoose.connect('mongodb://localhost:27017/movies',
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+})
+mongoose.connection.once('open', () => {
+  console.log('connected to mongod...')
+})
